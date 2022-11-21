a0da33bd26a36ed528e8c1eb3f05fba9a8fcf1f8
mr-kelma <mr.kelma@gmail.com>
2022-11-21 12:56:06 +0400

    Initial Commit

---
+++ PaperApp_Category button/PaperApp_Category_buttonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaperApp_Category_buttonApp.swift
+//  PaperApp_Category button
+//
+//  Created by Valery Keplin on 21.11.22.
+//
+
+import SwiftUI
+
+@main
+struct PaperApp_Category_buttonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
